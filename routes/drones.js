d6fcc023a52451bdec996ceddcf181a11d59732c
Francois L <francoisly@ymail.com>
2021-07-23 02:50:05 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,73 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const droneModel = require("../models/Drone.model.js")
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  droneModel.find()
+    .then((drones) => {
+      res.render("drones/list.hbs", {
+        drones: drones,
+      })
+    })
+    .catch(error => console.log(error))
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form.hbs")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  console.log(req.body);
+  droneModel.create(req.body)
+    .then(() => {
+      console.log("Insertion success")
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect("/drones/create")
+    })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  droneModel.findById(req.params.id)
+    .then((drone) => res.render("drones/update-form.hbs", {
+      drone: drone
+    }))
+    .catch(error => console.log(error))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  droneModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    .then(drone => {
+      console.log("update success")
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect(`/drones/${req.params.id}/edit`)
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  droneModel.findByIdAndDelete(req.params.id)
+  .then(drone => {
+    console.log("delete success")
+    res.redirect("/drones")
+  })
+  .catch(error => {
+    console.log(error);
+    res.redirect(`/drones`)
+  })
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,45 @@
 // Iteration #1
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+const droneModel = require("../models/Drone.model.js")
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
+
+const drones = [{
+     name: "Super Drone",
+     propellers: 6,
+     maxSpeed: 30
+    },
+    {
+        name: "Petit Drone",
+        propellers: 4,
+        maxSpeed: 15
+    },
+    {
+        name: "Moyen Drone",
+        propellers: 4,
+        maxSpeed: 20
+    }
+]
+
+droneModel.insertMany(drones)
+.then((data) => console.log("Create success, ", data.length, " inserted"))
+.catch(error => console.log(error))
+
